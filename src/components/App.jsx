5b8531ca35a44a2360e7cdca1ca02e23c9faf655
DimkaGrek <109220431+DimkaGrek@users.noreply.github.com>
2023-12-28 15:20:42 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,5 @@ import { BlogCard } from './BlogCard/BlogCard';
 import article from 'data/article.json';
 
 export const App = () => {
-  return (
-    <>
-      <BlogCard {...article} />
-    </>
-  );
+  return <BlogCard {...article} />;
 };
