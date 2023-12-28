eb222d34bb9245e09b0e4b3b3901965c70bbee93
Annazubakha <korepovaanna@gmail.com>
2023-12-28 13:40:46 +0100

    added folders

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { BlogCard } from './BlogCard/BlogCard';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div>App</div> <BlogCard />
+    </>
   );
 };
