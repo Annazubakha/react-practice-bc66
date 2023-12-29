7202de80973dcb2a544b1dd6a1b2391b10753942
VasylPopaz <pafos1997@gmail.com>
2023-12-29 13:30:09 +0100

    added normalize

---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Section = styled.section`
+  padding-top: ${({ theme }) => theme.spacing(20)};
+  padding-bottom: ${({ theme }) => theme.spacing(10)};
+`;
---
+++ src/styles/CreateGlobalStyle.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+  }
+
+  ul,
+  ol {
+    list-style: none;
+    padding-left: 0;
+    margin: 0;
+  }
+
+  button {
+    padding: 0;
+    border: none;
+    font: inherit;
+    color: inherit;
+    background-color: transparent;
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+`;
