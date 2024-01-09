9227af9bbd73c50682b55efb60c1391db69a700e
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-01-09 15:11:04 +0200

    Delete feature added

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,10 +1,27 @@
-export const TodoList = ({ todo }) => {
+export const TodoList = ({ todo, onDelete }) => {
   return (
-    <ul>
+    <ul style={{ display: 'flex', gap: '8px' }}>
       {todo.map(item => (
-        <li key={item.id}>
-          <span>{item.text}</span>
-          <button type="button">Delete</button>
+        <li
+          key={item.id}
+          style={{ padding: '10px', width: '150px', border: '1px solid black' }}
+        >
+          <span style={{ display: 'block', marginBottom: '5px' }}>
+            {item.text}
+          </span>
+          <button
+            style={{
+              display: 'block',
+              backgroundColor: '#ccc',
+              border: '1px solid black',
+              color: 'white',
+              marginLeft: 'auto',
+            }}
+            type="button"
+            onClick={() => onDelete(item.id)}
+          >
+            Delete
+          </button>
         </li>
       ))}
     </ul>
