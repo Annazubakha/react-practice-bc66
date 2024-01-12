d19442aa9ae624acdbbf6129c9b7b88b27837bcf
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-01-12 15:09:04 +0200

    task-1

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,26 +1,44 @@
-export const TodoList = ({ todo, onDelete }) => {
+import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+
+export const TodoList = ({ todo, onDelete, onEdit, disabled }) => {
   return (
     <ul style={{ display: 'flex', gap: '8px' }}>
       {todo.map(item => (
         <li
           key={item.id}
-          style={{ padding: '10px', width: '150px', border: '1px solid black' }}
+          style={{
+            padding: '10px',
+            width: '150px',
+            border: '1px solid black',
+            position: 'relative',
+          }}
         >
           <span style={{ display: 'block', marginBottom: '5px' }}>
             {item.text}
           </span>
           <button
             style={{
-              display: 'block',
-              backgroundColor: '#ccc',
-              border: '1px solid black',
-              color: 'white',
-              marginLeft: 'auto',
+              position: 'absolute',
+              right: 0,
+              top: 0,
             }}
             type="button"
             onClick={() => onDelete(item.id)}
+            disabled={disabled}
           >
-            Delete
+            <RiDeleteBinLine size={16} color={disabled ? 'grey' : 'red'} />
+          </button>
+          <button
+            type="button"
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: 0,
+            }}
+            disabled={disabled}
+            onClick={() => onEdit()}
+          >
+            <RiEdit2Line size={16} color={disabled ? 'grey' : 'lightblue'} />
           </button>
         </li>
       ))}
