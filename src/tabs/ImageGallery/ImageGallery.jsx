26ddcf10672bf01feb8d2a4a2e66939afa8ef987
Kovtunets Mykola <kovtunets73@gmail.com>
2024-01-15 14:10:18 +0100

    refactoring

--- src/tabs/ImageGallery/ImageGallery.jsx
+++ src/tabs/ImageGallery/ImageGallery.jsx
@@ -1,82 +1,82 @@
-import React, { Component } from 'react';
-import { Form } from 'components/Form/Form';
-import { getPhotos } from 'api/gallery';
-import { ImageGalleryList } from 'components/ImageGallery/ImageGalleryList';
-import { Container, Section } from 'components';
-import { Loader } from 'components/Loader/Loader';
-import Modal from 'components/Modal/Modal';
+import React, { useEffect, useState } from "react";
+import { Form } from "components/Form/Form";
+import { getPhotos } from "api/gallery";
+import { ImageGalleryList } from "components/ImageGallery/ImageGalleryList";
+import { Container, Section } from "components";
+import { Loader } from "components/Loader/Loader";
+import Modal from "components/Modal/Modal";
 
-export class ImageGallery extends Component {
-  state = {
-    photos: [],
-    query: '',
-    page: 1,
-    isLoadMore: false,
-    isEmpty: false,
-    error: null,
-    isLoading: false,
-    largeImgUrl: '',
+export const ImageGallery = () => {
+  const [photos, setPhotos] = useState([]);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [isLoadMore, setIsLoadMore] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [largeImgUrl, setLargeImgUrl] = useState("");
+
+  useEffect(() => {
+    if (!query) return;
+
+    setIsLoading(true);
+
+    (async () => {
+      try {
+        const { results, total } = await getPhotos(query, page);
+
+        if (!results.length) {
+          setIsEmpty(true);
+          return;
+        }
+
+        setPhotos(prev => [...prev, ...results]);
+        setIsLoadMore(page < Math.ceil(total / 20));
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, [query, page]);
+
+  const handleSubmit = query => {
+    setQuery(query);
+    setIsEmpty(false);
+    setPhotos([]);
+    setPage(1);
+    setError(null);
   };
 
-  componentDidUpdate(_, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.setState({ isLoading: true });
-      getPhotos(query, page)
-        .then(({ results, total }) => {
-          if (!results.length) {
-            this.setState({ isEmpty: true });
-            return;
-          }
-          this.setState(prev => ({
-            photos: [...prev.photos, ...results],
-            isLoadMore: page < Math.ceil(total / 20),
-          }));
-          console.log(results);
-        })
-        .catch(error => this.setState({ error: error.message }))
-        .finally(() => this.setState({ isLoading: false }));
-    }
-  }
-
-  handleSubmit = query => {
-    this.setState({ query, isEmpty: false, photos: [], page: 1, error: null });
-  };
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleLoadMore = () => {
+    setPage(prev => prev + 1);
   };
 
-  handleClickImg = (url = '') => {
-    this.setState({ largeImgUrl: url });
+  const handleClickImg = (url = "") => {
+    setLargeImgUrl(url);
   };
 
-  render() {
-    const { photos, isLoadMore, isEmpty, error, isLoading, largeImgUrl } =
-      this.state;
-    return (
-      <>
-        <Section>
-          <Container>
-            <Form addTodo={this.handleSubmit} text="Search photos" />
-            <ImageGalleryList photos={photos} openModal={this.handleClickImg} />
-            {isLoading && <Loader />}
-            {isLoadMore && (
-              <button onClick={this.handleLoadMore} type="button">
-                Load more
-              </button>
-            )}
-            {isEmpty && <p>No photos for your query!</p>}
-            {error && <p>Sorry, something went wrong {error}</p>}
-            {largeImgUrl && (
-              <Modal closeModal={this.handleClickImg}>
-                <img src={largeImgUrl} alt="" width="70%" />
-              </Modal>
-            )}
-          </Container>
-        </Section>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <Section>
+        <Container>
+          <Form addTodo={handleSubmit} text="Search photos" />
+          <ImageGalleryList photos={photos} openModal={handleClickImg} />
+          {isLoading && <Loader />}
+          {isLoadMore && (
+            <button onClick={handleLoadMore} type="button">
+              Load more
+            </button>
+          )}
+          {isEmpty && <p>No photos for your query!</p>}
+          {error && <p>Sorry, something went wrong {error}</p>}
+          {largeImgUrl && (
+            <Modal closeModal={handleClickImg}>
+              <img src={largeImgUrl} alt="" width="70%" />
+            </Modal>
+          )}
+        </Container>
+      </Section>
+    </>
+  );
+};
