027e44890ee590c9c7dfeb5c5324589658d033f0
Kovtunets Mykola <kovtunets73@gmail.com>
2024-01-19 15:25:39 +0100

    with ::after

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -19,38 +19,19 @@ export const Nav = styled.nav`
 
 export const List = styled.ul`
   display: flex;
-`;
 
-export const Indicator = styled.li`
-  background: ${({ theme }) => theme.colors.indicator};
-  position: absolute;
-  width: 70px;
-  height: 70px;
-  border-radius: 50%;
-  top: -50%;
-  border: 6px solid ${({ theme }) => theme.colors.blue};
-  transition: 0.5s;
-  &::before {
-    content: "";
-    position: absolute;
-    top: 46%;
-    left: -22px;
-    width: 20px;
-    height: 20px;
-    background: transparent;
-    border-top-right-radius: 20px;
-    box-shadow: 0 -10px 0 0 ${({ theme }) => theme.colors.blue};
-  }
   &::after {
     content: "";
+    background: ${({ theme }) => theme.colors.indicator};
     position: absolute;
-    top: 46%;
-    right: -22px;
-    width: 20px;
-    height: 20px;
-    background: transparent;
-    border-top-left-radius: 20px;
-    box-shadow: 0 -10px 0 0 ${({ theme }) => theme.colors.blue};
+    width: 70px;
+    height: 70px;
+    border-radius: 50%;
+    top: -50%;
+    border: 6px solid ${({ theme }) => theme.colors.blue};
+    transition: 0.5s;
+
+    transform: translateX(calc(70px * ${({ $selectedItem }) => $selectedItem}));
   }
 `;
 
@@ -72,27 +53,6 @@ export const Item = styled.li`
 
   width: 70px;
   height: 70px;
-  &:nth-child(1):has(.active)~.indicator {
-    transform: translateX(calc(70px * 0));
-  }
-  &:nth-child(2):has(.active)~.indicator {
-    transform: translateX(calc(70px * 1));
-  }
-  &:nth-child(3):has(.active)~.indicator {
-    transform: translateX(calc(70px * 2));
-  }
-  &:nth-child(4):has(.active)~.indicator {
-    transform: translateX(calc(70px * 3));
-  }
-  &:nth-child(5):has(.active)~.indicator {
-    transform: translateX(calc(70px * 4));
-  }
-  &:nth-child(6):has(.active)~.indicator {
-    transform: translateX(calc(70px * 5));
-  }
-  &:nth-child(7):has(.active)~.indicator {
-    transform: translateX(calc(70px * 6));
-  }
 `;
 
 export const StyledNavLink = styled(NavLink)`
