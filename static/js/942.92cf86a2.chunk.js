9beee5b1848b957dcb3580613b411d5ccd53c64f
Serhii-Fesianov <143389508+Serhii-Fesianov@users.noreply.github.com>
2024-01-25 12:44:12 +0000

    Deploying to gh-pages from @ Annazubakha/react-practice-bc66@65b04d86b7d564842bdf0681e21d68faed37917b 🚀

--- static/js/942.92cf86a2.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[942],{2942:function(t,e,H){H.d(e,{qy0:function(){return a}});var V=H(2119);function a(t){return(0,V.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"},child:[]}]})(t)}}}]);
-//# sourceMappingURL=942.92cf86a2.chunk.js.map
