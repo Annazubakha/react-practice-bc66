6776ef298922d6a8820c2a780e569a3684d11dca
Viacheslav Kostenko <kostenko.uvd@gmail.com>
2024-01-29 15:22:46 +0200

    added login, logout

--- src/myRedux/auth/operations.js
+++ src/myRedux/auth/operations.js
@@ -9,16 +9,44 @@ const setAuthHeader = token => {
   instance.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
+const clearAuthHeader = () => {
+  instance.defaults.headers.common.Authorization = ``;
+};
+
 // email: 'vasya123@email.com';
 // password: '12345678';
 export const userRegister = createAsyncThunk(
-  'auth/regusterUser',
+  'auth/registerUser',
   async (credentials, thunkApi) => {
-    console.log(credentials);
     try {
       const { data } = await instance.post('/users/signup', credentials);
       setAuthHeader(data.token);
       return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const userLogin = createAsyncThunk(
+  'auth/loginUser',
+  async (credentials, thunkApi) => {
+    try {
+      const { data } = await instance.post('/users/login', credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const userLogout = createAsyncThunk(
+  'auth/logoutUser',
+  async (_, thunkApi) => {
+    try {
+      await instance.post('/users/logout');
+      clearAuthHeader();
     } catch (error) {
       return thunkApi.rejectWithValue(error.message);
     }
--- src/myRedux/store.js
+++ src/myRedux/store.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { todoSliceReducer } from './todos/todoSlice';
 import { filterReducer } from './todos/filterSlice';
+import { authSliceReducer } from './auth/authSlice';
 
 const rootReducer = combineReducers({
+  auth: authSliceReducer,
   todos: todoSliceReducer,
   filter: filterReducer,
 });
